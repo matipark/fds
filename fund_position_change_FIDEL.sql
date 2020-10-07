99802cff983a1f3a4d381645969723e932e45f18
mpark <psj_1111@hotmail.com>
2020-10-07 17:03:37 +0800

    changes

--- fund_position_change_FIDEL.sql
+++ fund_position_change_FIDEL.sql
@@ -5,7 +5,7 @@ declare @histdate date
 declare @daysback integer
 declare @daysahead integer
 set @hldrid = '000KLZ-E'
-set @histdate = '2019-07-01'
+set @histdate = '2019-06-01'
 --as of date
 set @daysback = -730
 --number of days prior to the @histdate that the holdings would be considered valid
@@ -16,16 +16,19 @@ where price_date between dateadd(dd,@daysback,@histdate) and dateadd(dd,@daysahe
 
 
 
---CREATE TABLE sdfdemo_scratch.dbo.TAB_FIDEL_FF(factset_entity_id char(8) not null, entity_proper_name varchar(200) not null, fsym_id char(8) not null, proper_name varchar(200) not null, exchange varchar(40) null, country_desc varchar(24) null, l1_name varchar(200) null, l2_name varchar(200) null, l3_name varchar(200) null, l4_name varchar(200) null, position float null, mkt_val float null, report_date date null, as_of_date date not null, primary key (factset_entity_id, fsym_id, as_of_date))
+--CREATE TABLE sdfdemo_scratch.dbo.TAB_FIDEL_FF_2(factset_entity_id char(8) not null, entity_proper_name varchar(200) not null, turnover_label varchar(200) null, style varchar(200) null, objectives varchar(200) null, fsym_id char(8) not null, proper_name varchar(200) not null, exchange varchar(40) null, country_desc varchar(24) null, l1_name varchar(200) null, l2_name varchar(200) null, l3_name varchar(200) null, l4_name varchar(200) null, position float null, mkt_val float null, report_date date null, as_of_date date not null, primary key (factset_entity_id, fsym_id, as_of_date))
 
 --drop TABLE sdfdemo_scratch.dbo.TAB_FIDEL_FF
 
-insert into sdfdemo_scratch.dbo.TAB_FIDEL_FF
+insert into sdfdemo_scratch.dbo.TAB_FIDEL_FF_2
 
 select --top 100
 
 ent.factset_entity_id
 , ent.entity_proper_name
+, f.turnover_label
+, f.style
+, map.invt_obj_specialization_desc
 , h.fsym_id
 , sym.proper_name
 , coe.fref_listing_exchange exchange
@@ -51,7 +54,7 @@ group by fh.factset_fund_id
 ) md on md.max_date = h.report_date and md.factset_fund_id = h.factset_fund_id
 
 
-join sym_v1.sym_entity ent on f.Factset_Fund_ID = ent.[factset_entity_id]
+join sym_v1.sym_entity ent on f.Factset_Fund_ID = ent.factset_entity_id
 join sym_v1.sym_coverage sym on sym.fsym_id = h.fsym_id
 join sym_v1.sym_coverage coe on coe.fsym_id = sym.fsym_primary_listing_id
 join sym_v1.sym_sec_entity en on sym.fsym_id = en.fsym_id
@@ -60,6 +63,8 @@ join ref_v2.country_map cou on cou.iso_country = en2.iso_country
 join rbics_v1.rbics_entity_focus rb on rb.factset_entity_id = en.factset_entity_id
 join rbics_v1.rbics_structure st on rb.l6_id = st.l6_id
 join own_v5.own_sec_prices p on p.fsym_id = h.fsym_id and p.price_date = @pricedate
-
+join own_v5.own_ent_fund_objectives obj on ent.factset_entity_id = obj.factset_fund_id
+join ref_v2.invt_obj_specialization_map map on obj.invt_obj_specialization_code = map.invt_obj_specialization_code
 where f.factset_inst_entity_id = @hldrid
 and h.adj_holding is not null and rb.end_date is null and st.end_date is null
+
