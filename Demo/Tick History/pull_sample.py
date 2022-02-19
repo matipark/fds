13208003e9b0c182f3f22f5b6adb5b01eaf46071
matipark <psj_1111@hotmail.com>
2022-02-19 10:22:55 +0800

    changes

---
+++ Demo/Tick History/pull_sample.py
@@ -0,0 +1,134 @@
+#%%
+# Import libraries
+
+import requests
+import time
+
+#%%
+
+# Create a connection object
+
+#ticker = 'TLT,TMF,TMV,TBT,TTT,UBT,HYS,HYG,SJB,UJB,EMLC,EMB,ISRWF,IJPBF,ISJPF,VANVF,VNQ,DRN,DRV,GSG,GSP,DBB,DBA,XLY,XLP,XLE,XLF,XLV,XLB,XLU,DIA,XOP,XME,EEM,INDA,SPY,IJH,IJR'
+
+ticker = 'SIXY,VIX,SIXR'
+
+iso_code = 'CBO'
+start_date = "2022-02-08"
+end_date = "2022-02-08"
+
+authorization = ('FDS_DEMO_FE-410734','06LE9ERlxk7vS5AIwHFmThwJo0oX6ojGUUDVthEg')
+output_dir = r'C:\Users\mpark\OneDrive - FactSet\Desktop\Sample\_'
+
+
+#%%
+
+# Data generation query
+
+tick_endpoint = 'https://api.factset.com/bulk-documents/tickhistory/v1/request-files'
+
+tick_request={
+  "ticker": ticker,
+  "iso_code": iso_code,
+  "fields": "%2A",
+  "start_date": start_date,
+  "end_date": end_date
+}
+
+headers = {'Accept': 'application/json','Content-Type': 'application/json'}
+
+#%%
+
+## DATA GENERATION REQUEST ##
+### CAUTION ###
+
+tick_response = requests.get(url = tick_endpoint, params=tick_request, auth = authorization, headers = headers, verify= True )
+
+if tick_response.status_code == 200:
+    req_id = tick_response.json()['requestId']
+    print('Status Code: {}'.format(tick_response.status_code))
+    print('Request ID: {}'.format(req_id))
+    print('Status: {}'.format(tick_response.json()['status']))
+else:
+    print('Status Code: {}'.format(tick_response.status_code))
+    print('Error: Check your API key')
+
+
+
+#%%
+
+## DATA GET REQUEST / STATUS CHECK ##
+def tick_get_response():
+    tick_get_endpoint = 'https://api.factset.com/bulk-documents/tickhistory/v1/get-files'
+
+    tick_get_request={
+    "requestId": req_id,
+    "_paginationLimit": 500,
+    "_paginationOffset": 0,
+    }
+
+    # this is not a function, it's a variable
+    tick_get = requests.get(url = tick_get_endpoint, params=tick_get_request, auth = authorization, headers = headers, verify= True )
+    return tick_get
+
+print('Status Code: {}'.format(tick_get_response().status_code))
+print('Request ID: {}'.format(req_id))
+print('Status: {}'.format(tick_get_response().json()['status']))
+
+#%%
+
+for z in range (100):
+    tick_get_static = tick_get_response()
+    print('Loop number: {}'.format(z))
+    if tick_get_static.status_code == 200:
+        status = tick_get_static.json()['status']
+        if status == 'Completed':
+            print('Status: Completed\n')
+            break
+        elif status == 'Declined':
+            print('Status: Declined\n')
+            break
+        else:
+            print('Status: {}\n'.format(status))
+            time.sleep(15)
+    else:
+        print('Status Code: {}'.format(tick_get_static.status_code))
+        print('Error: Rerunning')
+        time.sleep(5)
+
+
+#%%
+
+# Looping through the list of URLs / building 
+
+output = tick_get_response().json()
+files_num = len(output['data'])
+
+url_list = []
+
+for i in range(files_num):
+    a = output['data'][i]['url']
+    i+=1
+    url_list.append(a)
+    print('Status: {}'.format(i))
+
+print('Number of URLs: {}'.format(len(url_list)))
+
+#%%
+
+for i,j in enumerate(url_list):
+    r = requests.get(url=j)
+    if r.status_code == 200:
+        print (" {:d} of {:d} downloaded".format(i+1,len(url_list)))
+    else:
+        print ('Error')
+        #create filename
+    local_filename = output_dir + 'port_' + end_date + '_' +str(i+1) + '.csv.gz'
+
+    with open(local_filename, 'wb') as f:
+        for chunk in r.iter_content(chunk_size = 1024):
+            if chunk:
+                f.write(chunk)
+
+
+# %%
+
