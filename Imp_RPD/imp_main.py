2d2f8c32744ef4c2e3b3776bac563b32e613b43b
matipark <psj_1111@hotmail.com>
2022-02-24 19:14:22 +0800

    changes

--- Imp_RPD/imp_main.py
+++ Imp_RPD/imp_main.py
@@ -14,11 +14,12 @@ from selenium.webdriver.support import expected_conditions as EC
 
 username = 'pie_au'
 imp_package = 'Snowflake WM/R FX rates implementation'
-
 machine_sn = '1129032'
 notes = 'Implementation for fx rates from WM/R in snowflake'
 
-driver = webdriver.Chrome()
+options = webdriver.ChromeOptions()
+# options.add_argument("headless")
+driver = webdriver.Chrome(chrome_options=options)
 
 #%%
 
@@ -33,8 +34,8 @@ def file_rpd(username, imp_package, machine_sn, notes):
     driver.find_element_by_xpath('//*[@id="createTemplate"]/div[8]/div[2]/property/div/button[2]/span').click() # Medium
     driver.find_element_by_xpath('//*[@id="createTemplate"]/div[1]/div[2]/input').send_keys(title) # write title
 
-    driver.find_element_by_xpath('//*[@id="createTemplate"]/div[2]/div[2]/ui-productsuggest/input').send_keys('standard implementation') # search category
-    WebDriverWait(driver, 60).until(EC.element_to_be_clickable((By.XPATH,"//*[@id='createTemplate']/div[2]/div[2]/ui-productsuggest/div/table/tbody/tr[1]/td[1]/span"))).click() # select category
+    driver.find_element_by_xpath('//*[@id="createTemplate"]/div[2]/div[2]/ui-productsuggest/input').send_keys('Implementation - CTS Standard Datafeed') # search category
+    WebDriverWait(driver, 60).until(EC.element_to_be_clickable((By.XPATH,'//tr [@data-qa-id="product_43276"]'))).click() # select category
 
     # # explicit frame id
     # frame = driver.find_element_by_xpath('//iframe [@id]').get_attribute('id')
@@ -45,18 +46,24 @@ def file_rpd(username, imp_package, machine_sn, notes):
     WebDriverWait(driver, 60).until(EC.element_to_be_clickable((By.XPATH,'//*[@id="tinymce"]'))).send_keys('Machine SN: {}\nNotes: {}'.format(machine_sn,notes)) # typing content
     driver.switch_to.default_content() # get out of iframe
 
+    # WebDriverWait(driver, 60).until(EC.element_to_be_clickable((By.XPATH,'//*[@id="createTemplate"]/div[19]/div[2]/button[1]/span'))).click()
+
+    # file it
+    
+    WebDriverWait(driver, 60).until(EC.visibility_of_element_located((By.XPATH,'//*[@id="SelectedProducts"]/div[2]/div/product-details/div/div/div[1]/span/img[1]')))
+
     file_button = driver.find_element_by_xpath('//*[@id="createTemplate"]/div[19]/div[2]/button[1]/span')
     driver.execute_script("arguments[0].click();", file_button)
     
-    #add me to the RPD
+    # add me to the RPD
     add_me = WebDriverWait(driver, 60).until(EC.element_to_be_clickable((By.XPATH,'//*[@id="addMeButton"]')))
     driver.execute_script("arguments[0].click();", add_me)
-    #extract rpd number
+
+    # extract rpd number
     rpd_link = driver.find_element_by_xpath('//*[@id="summaryContent"]/div[1]/div[1]/table/tbody/tr/td[1]/a').get_attribute('href')
-    driver.close()
+
     return rpd_link
 
-#product_43276
 
 # %%
 
@@ -65,8 +72,6 @@ def file_rpd(username, imp_package, machine_sn, notes):
 # driver.find_element_by_xpath('//*[@id="mat-select-2"]/div/div[1]/span/span').click()
 # time.sleep(1)
 # driver.find_element_by_xpath('//*[@id="mat-option-16"]/span').click()
-
-
 
 
 # %%
--- Imp_RPD/import_excel.py
+++ Imp_RPD/import_excel.py
@@ -5,16 +5,16 @@ import pandas as pd
 import imp_main as p
 import time
 
+
 #%%
 
-df = pd.read_excel (r'C:\Users\mpark\OneDrive - FactSet\Desktop\in_progress.xlsx')
-
+df = pd.read_excel(r'C:\Users\mpark\OneDrive - FactSet\Desktop\in_progress.xlsx')
 
 df_na = df.dropna(subset=['Imp_rpd'])
 df_keep = df.drop(df_na.index)
 df_keep
 
-
+max = len(df.dropna(subset=['Imp_rpd']))+1
 
 #%%
 
@@ -25,29 +25,62 @@ for index, row in df_keep.iterrows():
     machine_sn = row[3]
     notes = row[5]
     print (username, imp_package, machine_sn, notes)
-    time.sleep (5)
     rpd_url = p.file_rpd(username, imp_package, machine_sn, notes)
-    time.sleep (5)
+    print (rpd_url)
+    rpd_number = rpd_url[-8:]
+    time.sleep (3)
+
+
 
 
 # %%
 
-
-
-
-
 #'http://is.factset.com/rpd/Summary.aspx?messageId='
 
+rpd_list = pd.DataFrame([[123455],[2432123],[254523]], columns = ['rpd_link'])
+
+
+#%%    
+
+
+with pd.ExcelWriter(r'C:\Users\mpark\OneDrive - FactSet\Desktop\in_progress.xlsx', engine='openpyxl', if_sheet_exists='overlay', mode='a') as writer:  
+    rpd_list.to_excel(writer, startcol = 1, startrow=max+1, header = False, index = False)
+
+
 
 
 
 
 #%%
 
-df_na = df.dropna(subset=['Imp_rpd'])
+# Content APi
+content_adj_row = loader_max_row + ondemand_max_row + 8
 
-df_keep = df.drop(df_na.index)
+df = pd.DataFrame(columns = ['Content API Usage'])
 
-df_keep
+df.to_excel(writer, sheet_name=username, startrow= content_adj_row, index=False)
+
+column_settings = [{'header': column} for column in content_df.columns]
+(content_max_row, content_max_col) = content_df.shape
+worksheet = writer.sheets[username]
+
+if (content_max_row or content_max_col) == 1:
+    pass
+else:
+    worksheet.add_table(content_adj_row + 1, 0, content_max_row + content_adj_row + 1, content_max_col - 1, {'columns': column_settings})
+
+
+content_df.to_excel(writer, sheet_name=username, startrow=content_adj_row + 2, header=False, index=False)
+
+    # Close the Pandas Excel writer and output the Excel file.
+    writer.save()
+
 
 #%%
+
+#%%
+
+# VBA Reference
+
+# https://stackoverflow.com/questions/45105388/moving-numpy-arrays-from-vba-to-python-and-back
+# https://stackoverflow.com/questions/54577459/how-to-import-pandas-into-the-python-com-object-for-vba
