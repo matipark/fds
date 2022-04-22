8f65a1ac379977778ba9beca36a0ccb062d6da96
matipark <psj_1111@hotmail.com>
2022-04-22 17:14:34 +0800

    sf

---
+++ SF.session.sql
@@ -0,0 +1,2 @@
+select top 100 *
+from "FDS"."SYM_V1"."SYM_COVERAGE"
--- insight_article/Insight_2/0.pull_dataframe_sf.py
+++ insight_article/Insight_2/0.pull_dataframe_sf.py
@@ -3,6 +3,8 @@
 import pandas as pd
 import pyodbc
 import time
+
+from scipy.fftpack import dstn
 import loadsql #functions to load sql queries
 
 
@@ -10,23 +12,27 @@ import loadsql #functions to load sql queries
 
 # connecting python with SQL 
 dsn = 'SF'
-pw = 'FDSSnowflake_S@1ES'
-connection_to_sql = pyodbc.connect('DSN={dsn_name};pwd={password}'.format(dsn_name = dsn, password = pw))
+connection_to_sql = pyodbc.connect('DSN={dsn_name}'.format(dsn_name = dsn))
 
 # how_many = connection_to_sql.getinfo(pyodbc.SQL_MAX_CONCURRENT_ACTIVITIES)
 # print(how_many)
 
 #%%
 
-p_date = '2022-04-07'
+p_date = '2022-04-21'
 bdx_board_char_type = 'Overall Board Characteristics'
 
 start_time = time.time()
 
-sql_query_1 = loadsql.get_sql_q('1.bdx_sf.sql',show=0,connection=dsn).format(p_date=p_date, bdx_board_char_type=bdx_board_char_type)
+sql_query_1 = loadsql.get_sql_q('1.bdx_sf.sql',show=0,connection=dsn, skipSubsCheck=1).format(p_date=p_date, bdx_board_char_type=bdx_board_char_type)
 df_1 = pd.read_sql(sql_query_1,connection_to_sql)
 #df_1['p_date'] = pd.to_datetime(df_1['p_date']).dt.date #, format='%Y-%m-%d')
 
 print("Process finished --- %s seconds ---" % (time.time() - start_time))
+
+# %%
+
+
+df_1
 
 # %%
