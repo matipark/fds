ed49fc544f712ffd1af82c9e3775dc7b791278d6
matipark <psj_1111@hotmail.com>
2022-04-22 19:31:00 +0800

    changes

--- insight_article/Insight_2/0.pull_dataframe_sf.py
+++ insight_article/Insight_2/0.pull_dataframe_sf.py
@@ -3,6 +3,7 @@
 import pandas as pd
 import pyodbc
 import time
+import warnings
 
 from scipy.fftpack import dstn
 import loadsql #functions to load sql queries
@@ -19,13 +20,14 @@ connection_to_sql = pyodbc.connect('DSN={dsn_name}'.format(dsn_name = dsn))
 
 #%%
 
-p_date = '2022-04-21'
 bdx_board_char_type = 'Overall Board Characteristics'
 
 start_time = time.time()
 
-sql_query_1 = loadsql.get_sql_q('1.bdx_sf.sql',show=0,connection=dsn, skipSubsCheck=1).format(p_date=p_date, bdx_board_char_type=bdx_board_char_type)
-df_1 = pd.read_sql(sql_query_1,connection_to_sql)
+sql_query_1 = loadsql.get_sql_q('1.bdx_sf.sql',show=0,connection=dsn, skipSubsCheck=1).format(bdx_board_char_type=bdx_board_char_type)
+with warnings.catch_warnings():
+    warnings.simplefilter('ignore', UserWarning)
+    df_1 = pd.read_sql(sql_query_1,connection_to_sql)
 #df_1['p_date'] = pd.to_datetime(df_1['p_date']).dt.date #, format='%Y-%m-%d')
 
 print("Process finished --- %s seconds ---" % (time.time() - start_time))
